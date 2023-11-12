4672075c016867b6068774c5cb45d6c6ed5e28c4
felix <felix@element6.io>
2023-11-11 21:52:54 -0500

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by felix on 2023-11-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by felix on 2023-11-11.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
