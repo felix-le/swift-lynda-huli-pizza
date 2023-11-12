45a3b8c8b7c42f033e74670dd549329a35b5e8b7
felix <felix@element6.io>
2023-11-12 08:23:25 -0500

    working

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    var orders:[Int] = [1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                Image("surfBanner")
+                    .resizable()
+                    .scaledToFit()
+                Text("Huli Pizza Company")
+                    .background()
+            }
+//            End hero banner
+            Image(systemName: orders.isEmpty ? "cart":"cart.circle.fill")
+            HStack {
+                Text("Order Pizza")
+                    .font(.title)
+                Spacer()
+            }
+            ScrollView{
+                ForEach(orders, id: \.self) { order in
+                    HStack(alignment: .firstTextBaseline) {
+                        Text("Your Order Item \(order):")
+                            .font(.headline)
+                        Spacer()
+                        Text(14.09, format: .currency(code: "USD"))
+                    }
+                }
+            }
+        
+            
+            
+            VStack{
+                Image(systemName: "rectangle.fill").font(.largeTitle)
+                Text("Margherita")
+                Text("Description")
+            }
+            
+            ScrollView{
+                ForEach(1...25, id: \.self) { item in
+                    HStack (alignment: .top, spacing: 15) {
+                        Image(systemName:"\(item).circle.fill").font(.largeTitle)
+                            .font(.title)
+                        VStack(alignment: .leading) {
+                            Text("Margherita")
+                            Text("Description")
+                        }
+
+                    }
+                }
+            }
+          
+     
+            Spacer()
         }
-        .padding()
+            .padding()
     }
 }
 
