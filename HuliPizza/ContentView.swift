ff9befcab6306acfbc02a7f59c19dedf854ad1be
felix <felix@element6.io>
2023-11-14 15:19:42 -0500

    done add dynamic data

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -8,36 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var orders:[Int] = [1,2,3,4,6]
-    var showOrders:Bool = false
+    var menu: [MenuItem]
+    @State private var orders: [OrderItem] = testOrders
+    @State private var showOrders: Bool = false
     var body: some View {
         VStack {
-            
+
             HeaderView()
                 .shadow(radius: 5)
+
+            HStack {
+                Text("\(orders.count) orders")
+                Spacer()
+                Button { showOrders.toggle() } label: {
+                    Image(systemName: showOrders ? "cart" : "menucard")
+                }
+            }.foregroundStyle(.white)
+                .font(.title2)
+
             if showOrders
             {
-                OrderView(orders: orders)
+                OrderView(orders: $orders)
                     .cornerRadius(10)
             }
-            else{
+            else {
                 MenuItemView()
                     .padding(5)
                     .background(.thinMaterial, in: RoundedRectangle(cornerRadius: 10))
-                MenuView()
-                
+                MenuView(menu:menu)
+
             }
             Spacer()
         }
-        .padding()
-        .background(.linearGradient(colors: [.cyan,Color("Surf"),Color("Sky"),.white], startPoint: .topLeading, endPoint: .bottom))
+            .padding()
+            .background(.linearGradient(colors: [.cyan, Color("Surf"), Color("Sky"), .white], startPoint: .topLeading, endPoint: .bottom))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            
+        ContentView(menu: MenuModel().menu)
+
     }
 }
 
--- HuliPizza/MenuItemView.swift
+++ HuliPizza/MenuItemView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct MenuItemView: View {
+    @State private var addedItem:Bool = false
+    
     var body: some View {
         VStack {
             HStack {
@@ -45,6 +47,18 @@ struct MenuItemView: View {
                 }
                 
             }
+            
+            Button{
+                addedItem = true
+            }label: {
+                Spacer()
+                Text(12.99, format: .currency(code: "USD")).bold()
+                Image(systemName: addedItem ? "cart.fill.badge.plus": "cart.badge.plus" )
+                Spacer()
+            }.padding()
+                .background(.red, in: Capsule())
+                .foregroundStyle(.white)
+                .padding(5 )
         }
     }
 }
